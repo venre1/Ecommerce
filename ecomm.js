f03e4dc6833680c00487eaa6d5a37b450c9e1492
venre1 <128736471+venre1@users.noreply.github.com>
2025-05-07 01:39:03 +0100

    runapp commit

--- ecomm.js
+++ ecomm.js
@@ -1,23 +1,22 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
-const cookieSession =require('cookie-session');
+const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
-const AdminproductsRouter= require('./routes/admin/product');
+const AdminproductsRouter = require('./routes/admin/product');
 const productsRouter = require('./routes/productspage');
 const cartsRouter = require('./routes/cartsroute');
 
-const app  = express();
+const app = express();
 
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({extended:true})); 
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieSession({
-keys:['baf5jdk5sl6md3vnhjfv9']
+  keys: ['baf5jdk5sl6md3vnhjfv9']
 }));
- app.use(authRouter);
- app.use(productsRouter);
- app.use(AdminproductsRouter);
- app.use(cartsRouter);
-app.listen(3003,() =>{
-  console.log('Listening');
-});
+
+app.use(authRouter);
+app.use(productsRouter);
+app.use(AdminproductsRouter);
+app.use(cartsRouter);
+
+module.exports = app;
