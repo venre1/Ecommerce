d6c8fb1d49445e02632d887b57e7b1b73e9e5e8d
venre1 <128736471+venre1@users.noreply.github.com>
2025-05-09 00:57:24 +0100

    tmp file commit

--- ecomm.js
+++ ecomm.js
@@ -4,10 +4,11 @@ const authRouter = require('./routes/admin/auth');
 const AdminproductsRouter = require('./routes/admin/product');
 const productsRouter = require('./routes/productspage');
 const cartsRouter = require('./routes/cartsroute');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static('public'));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieSession({
   keys: ['baf5jdk5sl6md3vnhjfv9']
